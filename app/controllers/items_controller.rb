9ba47a6efa12a67c184255e23c9e383bbe5e2a4a
hitosi0317 <kondahitosi@gmail.com>
2021-04-06 15:00:37 +0900

    商品購入機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :order_index, only: [:edit, :update]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :order_index, only: [:edit, :update]
   before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
@@ -22,7 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    
+
   end
 
   def edit
